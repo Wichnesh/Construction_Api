c48afab157f7c30ee1cfd90e6b4ce082f615c4e2
Wichnesh <svignesh1999.sv@gmail.com>
2024-03-23 09:41:46 +0530

    password length update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ pool.query(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  password VARCHAR(50) NOT NULL,
+  password VARCHAR(132) NOT NULL,
   account_type INT
 )`,
   (err) => {
